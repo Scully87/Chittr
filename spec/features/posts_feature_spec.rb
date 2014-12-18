30b987ce6fd0483ac471d836599ab0c59f045e1c
Scully87 <scully87.ts@googlemail.com>
2014-12-18 20:42:28 +0000

    display a post

--- spec/features/posts_feature_spec.rb
+++ spec/features/posts_feature_spec.rb
@@ -4,8 +4,20 @@ feature 'posts' do
   context 'no posts have been added' do
     scenario 'should display a prompt to add a post' do
       visit '/posts'
-      expect(page).to have_content 'Chitts:'
-      expect(page).to have_link 'New Post'
+      expect(page).to have_content 'Posts'
+      expect(page).to have_link 'New'
     end
   end
+
+  context 'posts have been added' do
+  	before do
+  		Post.create(title: 'Hello')
+  	end
+
+	  scenario 'display posts' do
+	  	visit '/posts'
+	  	expect(page).to have_content('Hello')
+	  end
+	end
+
 end
