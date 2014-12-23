2ba1600bbe683480eeb5536c5e02e5123d11ed78
Scully87 <scully87.ts@googlemail.com>
2014-12-23 17:32:09 +0000

    belongs to, has many

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
-	
+
+	belongs_to :user
 	has_many :likes
 
 	has_attached_file :image, :styles => { :thumb => '75x75#', :large => '300x300#' }, :default_style => :thumb, :default_url => "http://www.whatthesupp.com/images/noImageAvailable300.gif"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :posts
 	devise :omniauthable, :omniauth_providers => [:facebook]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
