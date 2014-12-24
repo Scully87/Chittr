641f6f56e9ebb2477b9dc81dae3249a4765d4044
Scully87 <scully87.ts@googlemail.com>
2014-12-24 22:37:46 +0000

    fixed test bug with do and end oops

--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -5,7 +5,7 @@ include ApplicationHelper
 
 feature 'liking posts' do
 
-  context 'posts have been added'
+  context 'posts have been added' do
     scenario 'a user can like a post, which updates the post like count', js: true do
       sign_up
       create_post
@@ -20,4 +20,6 @@ feature 'liking posts' do
   	  click_link 'Like'
   	  expect(page).to have_content("1 likes")
   	end
+  end
+
 end
--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -6,27 +6,24 @@ include ApplicationHelper
 feature 'users' do
 
   context "not signed in and on the homepage" do
-    it "should see a 'sign in' link and a 'sign up' link" do
+    scenario "should see a 'sign in' link and a 'sign up' link" do
       visit('/')
       expect(page).to have_link('Sign in')
       expect(page).to have_link('Sign up')
     end
-
-    it "should not see 'sign out' link" do
+    scenario "should not see 'sign out' link" do
       visit('/')
       expect(page).not_to have_link('Sign out')
     end
   end
 
   context "signed in on the homepage" do
-
-    it "should see 'sign out' link" do
+    scenario "should see 'sign out' link" do
       sign_up
       visit('/')
       expect(page).to have_link('Sign out')
     end
-
-    it "should not see a 'sign in' link and a 'sign up' link" do
+    scenario "should not see a 'sign in' link and a 'sign up' link" do
       sign_up
       visit('/')
       expect(page).not_to have_link('Sign in')
