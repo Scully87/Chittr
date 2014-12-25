73fd6e2c66702bd5f303384e706ae6dcf25f4abd
Scully87 <scully87.ts@googlemail.com>
2014-12-25 00:29:26 +0000

    added likes into posts table in html

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
     get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
   end
