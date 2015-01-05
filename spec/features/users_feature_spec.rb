968fd78497342759c66e2b0fef7aea24b19d3c92
Scully87 <scully87.ts@googlemail.com>
2015-01-05 18:03:40 +0000

    created tests for signing up with image, successful test passes, unsuccessful sign up in progress

--- spec/features/users_feature_spec.rb
+++ spec/features/users_feature_spec.rb
@@ -31,4 +31,29 @@ feature 'users' do
     end
   end
 
+  context "must upload avatar when signing up" do
+    scenario "successfully sign up with avatar" do
+      visit('/')
+      click_link('Sign up')
+      fill_in('Name', with: 'Bob')
+      fill_in('Email', with: 'test@example.com')
+      fill_in('Password (8 characters)', with: 'testtest')
+      fill_in('Password Confirmation', with: 'testtest')
+      attach_file('user[avatar]', 'spec/features/user.jpeg')
+      click_button('Sign up')
+      expect(current_path).to eq '/'
+    end
+    xscenario "unable to sign up becasuse no avatar has been uploaded" do
+      visit('/')
+      click_link('Sign up')
+      fill_in('Name', with: 'Bob')
+      fill_in('Email', with: 'test@example.com')
+      fill_in('Password (8 characters)', with: 'testtest')
+      fill_in('Password Confirmation', with: 'testtest')
+      click_button('Sign up')
+      save_and_open_page
+      expect(current_path).to eq '/users/sign_up'
+    end
+  end
+
 end
