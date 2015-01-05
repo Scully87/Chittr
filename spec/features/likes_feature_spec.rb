adf38062bc9bf3b1eed709bae198cf0c4f90c5ee
Scully87 <scully87.ts@googlemail.com>
2015-01-05 16:49:56 +0000

    fixed tests, sign up comes with avatar attachment

--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -9,13 +9,12 @@ feature 'liking posts' do
     scenario 'a user can like a post, which updates the post like count', js: true do
       sign_up
       create_post
-
       visit '/posts'
       click_link 'Like'
       expect(page).to have_content("1 likes")
     end
     scenario 'a user can like a post, which increments the like count', js: true do
-  	  sign_up
+      sign_up
       create_post
       visit '/posts'
   	  click_link 'Like'
--- spec/helpers/application.rb
+++ spec/helpers/application.rb
@@ -1,18 +1,18 @@
 module ApplicationHelper
 
-	def sign_up
+  def sign_up
     visit('/')
     click_link('Sign up')
     fill_in('Name', with: 'Bob')
     fill_in('Email', with: 'test@example.com')
     fill_in('Password (8 characters)', with: 'testtest')
     fill_in('Password Confirmation', with: 'testtest')
+    attach_file('user[avatar]', 'spec/features/user.jpeg')
     click_button('Sign up')
   end
 
   def create_post
-  	visit '/posts/new'
-    # click_link 'New Post'
+    visit '/posts/new'
     fill_in 'Description', with: 'Hello'
     click_button 'Create Post'
     expect(page).to have_content 'Hello'
